3b8e905613251e691576ceed2da1792800554618
Saamir <sh308@uowmail.edu.au>
2021-07-04 17:30:14 +1000

    update faqs
    updated general structure

--- src/components/FAQS/FaqSection.js
+++ src/components/FAQS/FaqSection.js
@@ -6,6 +6,14 @@ function FaqSection() {
     <>
       <div className="faqs__container">
         <h1>Frequently Asked Questions</h1>
+        <ul className="faqs__list">
+          <li className="faqs__item">Question 1?</li>
+          <li className="faqs__item">Answer 1.</li>
+          <li className="faqs__item">Question 2?</li>
+          <li className="faqs__item">Answer 2.</li>
+          <li className="faqs__item">Question 3?</li>
+          <li className="faqs__item">Answer 3.</li>
+        </ul>
       </div>
     </>
   );
