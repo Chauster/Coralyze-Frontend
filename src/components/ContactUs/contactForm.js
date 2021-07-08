8c1b2a1f1cc723656328703525c3fe39d3b4484f
Saamir <sh308@uowmail.edu.au>
2021-07-08 11:24:03 +1000

    updated form validation
    - valids email addresses using regex
    - checks for domain after the @ symbol
    - validation for phone numbers, to be whole number integers

--- src/components/ContactUs/contactForm.js
+++ src/components/ContactUs/contactForm.js
@@ -61,6 +61,8 @@ function ContactForm() {
               </label>
               <input
                 required
+                title="The domain portion of the email address is invalid (the portion after the @)."
+                pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$"
                 id="email"
                 type="email"
                 name="email"
@@ -77,8 +79,9 @@ function ContactForm() {
               <input
                 required
                 minLength="10"
+                pattern="[-+]?[0-9]"
                 id="phone"
-                type="phone"
+                type="number"
                 name="phone"
                 className="form__input"
                 placeholder="Enter your contact number"
--- src/components/Register/registerFormSection.js
+++ src/components/Register/registerFormSection.js
@@ -203,6 +203,8 @@ function RegisterFormSection() {
             </div>
             <input
               required
+              title="The domain portion of the email address is invalid (the portion after the @)."
+              pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$"
               id="email"
               type="email"
               name="email"
@@ -224,8 +226,9 @@ function RegisterFormSection() {
             <input
               required
               minLength="10"
+              pattern="[-+]?[0-9]"
               id="phnumber"
-              type="text"
+              type="number"
               name="phnumber"
               className="form__input"
               placeholder="Enter your phone number"
