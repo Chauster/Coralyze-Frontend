6b677a5fba3cc367e5ee067216aed15a48798bc5
Chauster <chauderz@hotmail.com>
2021-07-19 01:19:21 +1000

    Added Dashboard Chart Functionality
    - You will need to install recharts for the charts to display.
    - yarn install recharts
    - npm install recharts

--- src/components/Dashboard/DashboardNavbar.js
+++ src/components/Dashboard/DashboardNavbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DashboardNavbar.scss";
+import { Link } from 'react-router-dom';
 import { RiDashboardLine } from "react-icons/ri";
 import { BiDevices, BiSupport } from "react-icons/bi";
 import { IoMdNotificationsOutline } from "react-icons/io";
@@ -10,49 +11,50 @@ const DashboardNavbar = () => {
   return (
     <div>
       <nav class="dashnavbar">
-        <div class="dashnavbar__logo">
-          <a href="/dashboard">
-            <img src={logo} alt="Coralyze Logo"></img>
-          </a>
-        </div>
-        <ul class="dashnavbar__links">
-          <li>
-            <a href="/dashboard">
-              <RiDashboardLine class="dashnavbar__links__icon" />
-              <span class="dashnavbar__links__label">Dashboard</span>
-            </a>
-            {/* <span class="dashnavbar__links__tooltip">Dashboard</span> */}
-          </li>
-          <li>
-            <a href="/devices">
-              <BiDevices class="dashnavbar__links__icon" />
-              <span class="dashnavbar__links__label">Devices</span>
-            </a>
-            {/* <span class="dashnavbar__links__tooltip">Devices</span> */}
-          </li>
-          <li>
-            <a href="/notifications">
-              <IoMdNotificationsOutline class="dashnavbar__links__icon" />
-              <span class="dashnavbar__links__label">Notifications</span>
-            </a>
-            {/* <span class="dashnavbar__links__tooltip">Notifications</span> */}
-          </li>
-          <li>
-            <a href="/confirmaccount">
-              <IoSettingsOutline class="dashnavbar__links__icon" />
-              <span class="dashnavbar__links__label">Settings</span>
-            </a>
-            {/* <span class="dashnavbar__links__tooltip">Settings</span> */}
-          </li>
-          <li>
-            <a href="/support">
-              <BiSupport class="dashnavbar__links__icon" />
-              <span class="dashnavbar__links__label">Ticket Support</span>
-            </a>
-            {/* <span class="dashnavbar__links__tooltip">Ticket Support</span> */}
-          </li>
-        </ul>
-      </nav>
+            <div class="dashnavbar__logo">
+                <Link to="/dashboard">
+                    <img src={logo} alt="Coralyze Logo"></img>
+                </Link>
+            </div>
+            <ul class="dashnavbar__links">
+            <li>
+                <Link to="/dashboard">
+                    <RiDashboardLine class="dashnavbar__links__icon"/>
+                    <span class="dashnavbar__links__label">Dashboard</span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/devices">
+                    <BiDevices class="dashnavbar__links__icon"/>
+                    <span class="dashnavbar__links__label">Devices</span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/notifications">
+                    <IoMdNotificationsOutline class="dashnavbar__links__icon"/>
+                    <span class="dashnavbar__links__label">Notifications</span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/confirmaccount">
+                <IoSettingsOutline class="dashnavbar__links__icon" />
+                <span class="dashnavbar__links__label">Settings</span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/support">
+                    <BiSupport class="dashnavbar__links__icon"/>
+                    <span class="dashnavbar__links__label">Ticket Support</span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/logout">
+                    <BiSupport class="dashnavbar__links__icon"/>
+                    <span class="dashnavbar__links__label">Logout</span>
+                </Link>
+            </li>
+            </ul>
+        </nav>
 
       {/* Below is placeholder for now, to be removed later, this is to primarily test the dashnavbar and content widths */}
       {/* <div class="dashboard">
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DashboardNavbar from '../components/Dashboard/DashboardNavbar';
+import Dashboard from '../components/Dashboard/Dashboard';
 
 const dashboard = () => {
   return (
     <>
       <DashboardNavbar></DashboardNavbar>
+      <Dashboard></Dashboard>
     </>
   );
 };
