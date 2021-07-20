544d63b0c0a48737581532d6822cd6a699be020a
kishenkumar345 <kishen.kumar345@gmail.com>
2021-07-21 01:31:28 +1000

    Fixed notification overflow, overlapping of props

--- src/components/Dashboard/NotificationsSection.js
+++ src/components/Dashboard/NotificationsSection.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "../Button";
 import "./NotificationsSection.css";
 
 function NotificationsSection() {
@@ -7,22 +6,46 @@ function NotificationsSection() {
   const Notification = (props) => {
 
     return (
-      <div className="notif">
+      <li className="notif">
         <h3 class="subject">{props.subject}</h3>
         <p class="description">{props.description}</p>
-      </div>
+      </li>
     );
   }
 
   return (
     <div className="main_section">
       <h1 class="main_title">Notifications</h1>
-        <div className="notif_box">
+        <ul className="notif_box">
           <Notification
           subject="Account Issue"
           description="Some issue occured.">
           </Notification>
-        </div>
+          <Notification
+          subject="Account Issue"
+          description="Some issue occured.">
+          </Notification>
+          <Notification
+          subject="Account Issue"
+          description="Some issue occured.">
+          </Notification>
+          <Notification
+          subject="Account Issue"
+          description="Some issue occured.">
+          </Notification>
+          <Notification
+          subject="Account Issue"
+          description="Some issue occured.">
+          </Notification>
+          <Notification
+          subject="Account Issue"
+          description="Some issue occured.">
+          </Notification>
+          <Notification
+          subject="Account Issue"
+          description="Some issue occured.">
+          </Notification>
+        </ul>
     </div>
   );
 }
