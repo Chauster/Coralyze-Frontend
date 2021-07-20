e1c9494801cc2e3cf110e391583a4a01b647b24b
Saamir <sh308@uowmail.edu.au>
2021-07-20 23:45:02 +1000

    updated css for dashboard settings
    - fixed issues regarding overflow on both confirm account and change account settings pages
    - minimal fix to reactive page elements

--- src/components/Dashboard/ConfirmAccountSection.js
+++ src/components/Dashboard/ConfirmAccountSection.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { Button } from "../Button";
-import "./SettingsSection.css";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Button } from '../Button';
+import './ConfirmAccountSection.css';
 
-function SettingsSection() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+function ConfirmAccountSection() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   let handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -30,71 +30,69 @@ function SettingsSection() {
       .get(`http://localhost:5000/users/authenticate/${username}`)
       .then((res) => {
         res.data.password === user.password
-          ? (window.location = "/settings")
+          ? (window.location = '/settings')
           : alert(`Incorrect Username or Password`);
       })
       .catch((err) => {
         alert(`User doesn't exist!`);
         // console.log("User doesn't exist");
-        console.log("Error: " + err);
+        console.log('Error: ' + err);
       });
 
     // set fields to empty after submit
-    // link to settingsSection on dashboard after success
+    // link to ConfirmAccountSection on dashboard after success
   };
 
   return (
-    <>
-      <div className="main__section">
-        <form autoComplete="off" className="form" onSubmit={handleSubmit}>
-          <h1 class="form__title">Change Account Details</h1>
-          <div className="form__inputs">
-            <div class="form__subtitle">
-              <label htmlFor="username" className="form__label">
-                Username
-              </label>
-            </div>
-            <input
-              required
-              minLength="3"
-              id="username"
-              type="text"
-              name="username"
-              className="form__input"
-              placeholder="Enter your username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
+    <div className="main__section">
+      <form autoComplete="off" className="form" onSubmit={handleSubmit}>
+        <h1 class="form__title">Confirm Account Details</h1>
+        <div className="form__inputs">
+          <div class="form__subtitle">
+            <label htmlFor="username" className="form__label">
+              Username
+            </label>
           </div>
-          <div className="form__inputs">
-            <div class="form__subtitle">
-              <label htmlFor="password" className="form__label">
-                Password
-              </label>
-            </div>
-            <input
-              required
-              minLength="8"
-              pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
-              title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."
-              id="password"
-              type="password"
-              name="password"
-              className="form__input"
-              placeholder="Enter your password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
+          <input
+            required
+            minLength="3"
+            id="username"
+            type="text"
+            name="username"
+            className="form__input"
+            placeholder="Enter your username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form__inputs">
+          <div class="form__subtitle">
+            <label htmlFor="password" className="form__label">
+              Password
+            </label>
           </div>
-          <div className="form__btn">
-            <Button buttonSize="btn--wide" buttonColor="blue" type="submit">
-              Continue
-            </Button>
-          </div>
-        </form>
-      </div>
-    </>
+          <input
+            required
+            minLength="8"
+            pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
+            title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."
+            id="password"
+            type="password"
+            name="password"
+            className="form__input"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className="form__btn">
+          <Button buttonSize="btn--wide" buttonColor="blue" type="submit">
+            Continue
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
 
-export default SettingsSection;
+export default ConfirmAccountSection;
