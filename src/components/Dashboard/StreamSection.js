28bc042a8d0c5094ffc6c0805b0a69930fb58dd4
Saamir <sh308@uowmail.edu.au>
2021-07-21 23:26:16 +1000

    updated streamSection.js
    - added line break to drop down video

--- src/components/Dashboard/StreamSection.js
+++ src/components/Dashboard/StreamSection.js
@@ -8,6 +8,7 @@ function StreamSection() {
       <div className="stream__container">
         <h3>Video Stream</h3>
         please wait...
+        <br />
         <video className="video__stream" controls autoPlay>
           <source src={stream} type="video/mp4" />
           Your browser does not support the video tag.
