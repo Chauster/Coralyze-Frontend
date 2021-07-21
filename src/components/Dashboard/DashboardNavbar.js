ec5ec238983245e6865b3ea288e4fecabf9639e8
Saamir <sh308@uowmail.edu.au>
2021-07-21 16:40:44 +1000

    updated dashboard components and navbar
    - updated navbar to drop to bottom of page when size is below 960px
    - fixed dashboard pages to fit to page width when page size is reduced
    - bottom navbar logo and label display set to none

--- src/components/Dashboard/DashboardNavbar.js
+++ src/components/Dashboard/DashboardNavbar.js
@@ -1,60 +1,60 @@
-import React from "react";
-import "./DashboardNavbar.scss";
+import React from 'react';
+import './DashboardNavbar.scss';
 import { Link } from 'react-router-dom';
-import { RiDashboardLine } from "react-icons/ri";
-import { BiDevices, BiSupport, BiLogOut } from "react-icons/bi";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { IoSettingsOutline } from "react-icons/io5";
-import logo from "../../images/logo-2.png";
+import { RiDashboardLine } from 'react-icons/ri';
+import { BiDevices, BiSupport, BiLogOut } from 'react-icons/bi';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { IoSettingsOutline } from 'react-icons/io5';
+import logo from '../../images/logo-2.png';
 
 const DashboardNavbar = () => {
   return (
     <div>
       <nav class="dashnavbar">
-            <div class="dashnavbar__logo">
-                <Link to="/dashboard">
-                    <img src={logo} alt="Coralyze Logo"></img>
-                </Link>
-            </div>
-            <ul class="dashnavbar__links">
-            <li>
-                <Link to="/dashboard">
-                    <RiDashboardLine class="dashnavbar__links__icon"/>
-                    <span class="dashnavbar__links__label">Dashboard</span>
-                </Link>
-            </li>
-            <li>
-                <Link to="/devices">
-                    <BiDevices class="dashnavbar__links__icon"/>
-                    <span class="dashnavbar__links__label">Devices</span>
-                </Link>
-            </li>
-            <li>
-                <Link to="/notifications">
-                    <IoMdNotificationsOutline class="dashnavbar__links__icon"/>
-                    <span class="dashnavbar__links__label">Notifications</span>
-                </Link>
-            </li>
-            <li>
-                <Link to="/confirmaccount">
-                <IoSettingsOutline class="dashnavbar__links__icon" />
-                <span class="dashnavbar__links__label">Settings</span>
-                </Link>
-            </li>
-            <li>
-                <Link to="/support">
-                    <BiSupport class="dashnavbar__links__icon"/>
-                    <span class="dashnavbar__links__label">Support</span>
-                </Link>
-            </li>
-            <li>
-                <Link to="/logout">
-                    <BiLogOut class="dashnavbar__links__icon"/>
-                    <span class="dashnavbar__links__label">Logout</span>
-                </Link>
-            </li>
-            </ul>
-        </nav>
+        <div class="dashnavbar__logo">
+          <Link to="/dashboard">
+            <img src={logo} alt="Coralyze Logo"></img>
+          </Link>
+        </div>
+        <ul class="dashnavbar__links">
+          <li>
+            <Link to="/dashboard">
+              <RiDashboardLine class="dashnavbar__links__icon" />
+              <span class="dashnavbar__links__label">Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/devices">
+              <BiDevices class="dashnavbar__links__icon" />
+              <span class="dashnavbar__links__label">Devices</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/notifications">
+              <IoMdNotificationsOutline class="dashnavbar__links__icon" />
+              <span class="dashnavbar__links__label">Notifications</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/confirmaccount">
+              <IoSettingsOutline class="dashnavbar__links__icon" />
+              <span class="dashnavbar__links__label">Settings</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/support">
+              <BiSupport class="dashnavbar__links__icon" />
+              <span class="dashnavbar__links__label">Support</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/logout">
+              <BiLogOut class="dashnavbar__links__icon" />
+              <span class="dashnavbar__links__label">Logout</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
       {/* Below is placeholder for now, to be removed later, this is to primarily test the dashnavbar and content widths */}
       {/* <div class="dashboard">
