860e801777cbfad6692f7c06741b8b340c04d5f1
Chauster <chauderz@hotmail.com>
2021-08-12 12:26:22 +1000

    New Logo navbar implementation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../images/logo-2.png';
+import logo from '../images/logo-white.svg';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Button } from './Button';
 import './Navbar.css';
